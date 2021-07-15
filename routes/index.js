6e1768931bf3a2d616075d1fc300ca7a0bb00ac0
Zihan Li <lizihan@umich.edu>
2021-07-15 00:23:28 -0700

    add driver

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,21 @@ var express = require('express');
 var router = express.Router();
 var utils = require('./utils');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Temperature Control' });
+function startHeater() {
   utils.start();
   utils.sleep(1000).then(() => {
     utils.timer();
   });
+}
+
+var startTime = new Date("2021/07/15 00:24:00").getTime()
+var currTime = new Date().getTime()
+
+setTimeout(startHeater, startTime - currTime);
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Temperature Control' });
 });
 
 module.exports = router;
